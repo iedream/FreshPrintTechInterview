415fa3618f43263f18ec0068592fd4193e70bdd8
Catherine Zhao <iedream@v1020-wn-14-205.campus-dynamic.uwaterloo.ca>
2016-05-31 17:32:00 -0400

    Final Copy

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,8 +1,17 @@
 'use strict';
-
+var http = require('http');
 /* Controller */
-angular.module('myApp.controllers', []).
+angular.module('myApp', []).
 	controller('AppCtrl', function ($scope, $http) {
-		// Insert controller code here
-		
-	});
+	// Insert controller code here
+	$scope.data = '4';
+	//http.get('/api/brandNames', function (res) {
+	//	res.on('data', function (data) {
+	//		// Parse response XML data here
+	//		$scope.data = data;
+	//	});
+	//}).on('error', function (error) {
+	//	// Handle EDI call errors here
+	//	$scope.error = 'data could not be loaded'
+	//});
+});
